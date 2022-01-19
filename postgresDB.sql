a4d0ee2fc3bf6e932f5e22526288499ddb564a11
Wong <hwongkf8@gmail.com>
2022-01-19 16:11:50 -0500

    added sql file

---
+++ postgresDB.sql
@@ -0,0 +1,74 @@
+
+-- Create the database
+
+CREATE DATABASE postgres;
+
+-- create a store table
+
+CREATE TABLE store(
+    store_id serial NOT NULL PRIMARY KEY,
+    store_name VARCHAR(50) NOT NULL,
+    location_city VARCHAR(50) NOT NULL,
+    is_open BOOLEAN NOT NULL,
+    store_phonenumber INTEGER NOT NUll );
+
+-- create a employees table with a foreign key referencing to the store_id
+
+CREATE TABLE employees(
+    employee_id serial NOT NULL PRIMARY KEY,
+    store_id INTEGER,
+    employee_name VARCHAR(50) NOT NULL,
+    employee_phonenumber INTEGER NOT NULL,
+    employee_email VARCHAR(100) NOT NULL,
+    employee_roll VARCHAR(30) NOT NUll,
+	FOREIGN KEY (store_id) REFERENCES store(store_id)
+);
+
+-- create a product table with foreign key referencing to the store_id
+
+Create table product(
+	product_id serial NOT NULL PRIMARY KEY,
+	store_id INTEGER,
+    FOREIGN KEY (store_id) REFERENCES store(store_id),
+	product_name varchar,
+	product_price int
+);
+
+-- create a purchases table with foreign key referencing to the store_id and product_id
+
+create table purchases(
+	purchase_id int NOT NULL PRIMARY KEY,
+	store_id int,
+	product_id int,
+	purchase_date date,
+	purchase_price int,
+	FOREIGN KEY (store_id) REFERENCES store(store_id),
+	FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+
+-- create a customers table with foreign key referencing to the purchase_id
+
+Create table customers (
+	customers_id serial NOT NULL PRIMARY KEY,
+	purchase_id int,
+	customers_name VARCHAR(50) NOT NULL,
+	customers_phonenumber int NOT NULL,
+	customers_address VARCHAR(200) NOT NULL,
+	foreign key (purchase_id) references purchases(purchase_id)
+);
+
+
+-- stack of inner joins that joins the 5 tables
+
+select *
+From store
+inner join employees
+on store.store_id = employees.store_id
+inner join product
+on store.store_id = product.store_id
+inner join purchases
+on store.store_id = purchases.store_id
+inner join customers
+on purchases.purchase_id = customers.purchase_id;
+
